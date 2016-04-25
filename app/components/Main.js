64cf6941b68cf40954b66e56e34e6b4813e32f6d
theperfectfuel <robert@theperfectfuel.com>
2016-04-25 02:19:10 -0700

    Passing props between components

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,42 +2,41 @@ var React = require('react');
 
 var ReactDOM = require('react-dom');
 
+var storesObj = {
+	stores: [
+		{storeName: "Trader Joes", items: ["Coffee", "Beer", "Beef Jerky"]}, 
+		{storeName: "Sprouts", items: ["Lettuce", "Nuts", "Cilantro"]}, 
+		{storeName: "Albertsons", items: ["Milk", "Meat"]}
+	]
+};
+
 var Board = React.createClass({
 
 	render: function() {
-
+		var stores = [];
+		for (var i = 0; i < 3; i++) {
+			stores.push(<Store storeName={storesObj.stores[i].storeName} items={storesObj.stores[i].items} />);
+			//console.log(storesObj.stores[0].items);
+		}
 		return (
 			<div className="board">
-				<Store />
+				{stores}
 			</div>
-
-		)
+		);
 	}
-
 });
 
-var Store = function() {
-	var storeName = "Trader Joes";
-	var storeName2 = "Sprouts";
-	var storeName3 = "Albertsons";
+var Store = function(props) {
+	var items = [];
+	for (var i = 0; i < props.items.length; i++) {
+		items.push(<Item itemName={props.items[i]} />);
+	}
+
 		return (
 			<div>
 				<div className="store">
-				{storeName}
-				<Item itemName="Coffee" />
-				<Item itemName="Beer" />
-				</div>
-
-				<div className="store">
-				{storeName2}
-				<Item itemName="Nuts" />
-				<Item itemName="Fruit" />
-				</div>
-
-				<div className="store">
-				{storeName3}
-				<Item itemName="Meat" />
-				<Item itemName="Eggs" />
+				{props.storeName}
+				{items}
 				</div>
 			</div>
 		)
